81ac09c817915c43b62a3564672764ac2d725690
IvanPetrov <ivan.petrov@gmail.com>
2025-06-30 00:31:51 +0400

    обновление task5.py

--- task5.py
+++ task5.py
@@ -5,4 +5,4 @@ for row in range(user_height):
     count += 1
     print(f"{' ' * (user_height - count)}{'#' * row}#{'#' * row}", end="")
     print()
-
+#Я могу решить с вложенным циклом, но с одним проще
